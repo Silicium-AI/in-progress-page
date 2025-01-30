3f85b3e8089b141b42df557a7a8ee9be6108bd18
jdhruv1503 <jdhruv1503@gmail.com>
2025-01-31 01:55:40 +0530

    Make smaller

--- app/page.js
+++ app/page.js
@@ -13,9 +13,9 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-gradient-to-br from-stone-200 to-stone-400 text-white flex flex-col items-center justify-center p-4">
       <div className="text-center">
-        <Card className="p-32 rounded-xl border-stone-400 border-2 shadow-lg bg-stone-300">
+        <Card className="p-24 rounded-xl border-stone-400 border-2 shadow-lg bg-stone-300">
         <div className="flex items-center justify-center gap-4 mb-32">
-          <div className="w-full h-96 relative">
+          <div className="w-full h-48 relative">
             <Image
               src="/in-progress-page/silicium-logo.svg"
               alt="Silicium Logo"
@@ -29,8 +29,8 @@ export default function Home() {
           <div className="absolute inset-2 border-4 border-r-black border-t-transparent border-b-transparent border-l-transparent rounded-full animate-spin-slow"></div>
         </div>
 
-        <h2 className="text-6xl font-bold mb-4 tracking-tighter text-black">This website is under construction!</h2>
-        <p className="text-gray-700 text-3xl tracking-tighter">
+        <h2 className="text-4xl font-bold mb-4 tracking-tighter text-black">This website is under construction!</h2>
+        <p className="text-gray-700 text-xl tracking-tighter">
           We're crafting something extraordinary. Stay tuned.
         </p>
         </Card>
